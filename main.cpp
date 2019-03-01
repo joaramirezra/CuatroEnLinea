b5efc78f91b8139d277edcf6efbc3763518bd36e
Johan <joaramirezra@unal.edu.co>
2019-03-01 10:10:43 -0500

    version 1.1 bug diagonal solucionado, tambien pulio el codigo

--- main.cpp
+++ main.cpp
@@ -1,21 +1,14 @@
 #include "miniwin.h" // libreria utilizada para los graficas,tamaño de venta, color, movilidad del juego
-#include "iostream" // libreria de entrada y salida de datos 
-#include "windows.h"
-#include "math.h"
-#include "sstream"
 
 using namespace miniwin;
 
 //variable globales
-
 const int numeroFilas = 6;
 const int numeroColumnas = 7;
 int size = 70;
 int Tablero [numeroFilas][numeroColumnas];
 int x,y,turno;
 bool enJuego ;
-
-void Configurarpartida();
 
 void LLenarTablero(){
 	for(int i = 0 ; i < numeroFilas ; i++){
@@ -37,7 +30,6 @@ bool VerificarGanador(){
 		}
 	}
 	// verifica si hay 4 fichas en linea en direccion vertical
-	
 	for(int j = 0 ; j < numeroColumnas  ; j++){
 		contador = 0;
 		for(int i = 0 ; i < numeroFilas   ; i++){
@@ -46,8 +38,7 @@ bool VerificarGanador(){
 			if (contador == 4)return true;
 		}
 	}	
-	
-	//ojo, la notacion hay que tenerla en razon a la matriz tablero, ya que en la parte grafica
+	//ojo, la notacion hay que tenerla en cuenta a la matriz tablero, ya que en la parte grafica
 	// el eje y esta invertido
 	// verifica si hay 4 fichas en linea en direccion diagonal ascendente (\)superior
 	for(int j = 0 ; j < numeroColumnas ; j++){
@@ -60,8 +51,7 @@ bool VerificarGanador(){
 			}
 		}
 	}
-	
-	// verifica si hay 4 fichas en linea en direccion diagonal ascendente (\)superior
+	// verifica si hay 4 fichas en linea en direccion diagonal ascendente (\)inferior
 	for(int i = 1 ; i < numeroFilas  ; i++){
 		contador = 0;
 		for(int j = 0 ; j <numeroColumnas  ; j++){
@@ -72,8 +62,7 @@ bool VerificarGanador(){
 			}
 		}
 	}
-	
-	// verifica si hay 4 fichas en linea en direccion diagonal ascendente (/)superior
+	// verifica si hay 4 fichas en linea en direccion diagonal descendente (/)superior
 	for(int i = numeroColumnas-1 ; i >= 0; i--){
 		contador = 0;
 		for(int j = 0 ; j < numeroFilas ; j++){
@@ -84,12 +73,12 @@ bool VerificarGanador(){
 			}
 		}
 	}
-	//verifica si hay 4 fichas en linea en direccion diagonal ascendente (/)inferior
-	for(int i = numeroFilas-2 ; i >= 0 ; i--){
+	//verifica si hay 4 fichas en linea en direccion diagonal desscendente (/)inferior
+	for(int i = 1 ; i < numeroFilas ; i++){
 		contador = 0;
-		for(int j = 0 ; j < numeroColumnas ; j++){
-			if( i - j >= 0){
-				if(Tablero[i-j][i] == turno%2 ) contador++;
+		for(int j = 0; j <numeroColumnas  ; j++){
+			if( i + j <numeroFilas){
+				if(Tablero[i+j][numeroColumnas-1-j] == turno%2 ) contador++;
 				else contador = 0;
 				if (contador == 4)return true;	
 			}
@@ -101,7 +90,7 @@ bool VerificarGanador(){
 int VerificarCasilla(int x ){
 	int counter = 0; // se inicializa un contador en cero, este se encarga de contar las fichas de manera vertical
 	while(Tablero[counter][x] !=-1 && counter < 6)counter++; //mientras la casilla este ocupada y sea menor que 6 (7 fichas) , aumente uno al contador
-	if(counter < 6) return counter; // una vez sale del contador, si el contador esta en el rango , es decir esta vqcia, devulve la poscion vertical donde se coloca
+	if(counter < 6) return counter; // una vez sale del while, si el contador esta en el rango , es decir esta vacia, retorna la poscion vertical donde se coloca
 	return -1; // en caso contrario se retorna -1 indicando que no hay espacio en esa columna 
 }
 
@@ -118,7 +107,6 @@ void DibujarTablero(){
 			Dibujarcasilla(i*size,j*size,size); 	
 		}
 	}
- 	refresca();
 }
 
 void colocarFicha(int x, int y,bool jugador){
@@ -132,7 +120,6 @@ void BorrarFicha(int x, int y){
 	circulo_lleno(x*size+size/2,size/2 + y*size,size/2);
 }
 
-
 void Configurarpartida(){
 	vredimensiona(500, 500); // se crea la ventana de 500 px x 500px
 	LLenarTablero(); // con -1
@@ -143,23 +130,7 @@ void Configurarpartida(){
 	turno =0;
 	colocarFicha(0,0,(turno%2==0));
 	refresca();		
-}
-
-void dibujarTableroMensaje(){
-	std::ostringstream ss;
-    
-    for(int i = numeroFilas-1 ; i>-1 ;i--){
-		for(int j = 0 ; j<numeroColumnas ; j++){
-			if(Tablero[i][j] == -1 ) ss<<" - ";
-			else if(Tablero[i][j] == 0 ) ss<<" 0 ";
-			if(Tablero[i][j] == 1 ) ss<<" X ";	
-		}
-		ss<<"\n";
-	}
-	mensaje(ss.str());
-}
-			
-
+}			
 
 int main (){
 	int entradaTeclado;	
@@ -178,11 +149,9 @@ int main (){
 		}
 		else if(entradaTeclado == ABAJO){
 			int validacion  = VerificarCasilla(x);
-			 
 			if(validacion != -1){
 				colocarFicha(x,6-validacion,(turno%2==0));
 				Tablero[validacion][x] = turno%2;
-				
 				if(VerificarGanador()){
 					BorrarFicha(x,0);
 					refresca();
@@ -190,17 +159,13 @@ int main (){
 					else mensaje("Gano el jugador rojo");
 					Configurarpartida();
 				}
-
 				else {
 					turno++;
 					BorrarFicha(x,y);
-					//x=0;
 					colocarFicha(x,0,(turno%2==0));
-					//DibujarTablero2();
 				}
 				refresca();
 			}
-				
 		}
 		if(turno>41){
 			mensaje("Partida Empatada");
@@ -208,39 +173,7 @@ int main (){
 		}
 		
 	}while(enJuego);
-	
+
 	return 0;
 }
 
-/*
-metodos para debugear
-
-
-void DibujarTableroConsola(){
-	for(int i = 0 ; i < numeroFilas ; i++){
-		for(int j = 0 ; j < numeroColumnas ; j++){
-			cout<<Tablero[i][j]<<" ";
-		}
-		cout<<endl;
-	}	
-}
-
-void colocarFichaDEBUG(int , int ,int );
-
-void DibujarTablero2(){
-	for(int i = 0 ; i < numeroFilas ; i++){
-		for(int j = 0 ; j < numeroColumnas ; j++){
-			colocarFichaDEBUG(j,6-i,Tablero[i][j]);
-		}
-	}
-}
-
-void colocarFichaDEBUG(int x, int y,int jugador){
-	if(jugador ==  0) color(VERDE); // jugador 1
-	else if(jugador ==  1) color(BLANCO); // jugador 2
-	else color(CYAN); 
-	circulo_lleno(x*size+size/2,size/2 + y*size,size/4);
-}
-
-
-*/
